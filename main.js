0606b83feb8cf4d90ddc7341d61cc775efe4e387
ThePywon <79721537+ThePywon@users.noreply.github.com>
2021-06-15 17:10:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-function Show(index)
-{
-  var div;
-  if(index == 1)
-    div = document.getElementById("bio");
-  else if(index == 2)
-    div = document.getElementById("projects");
-  
-  div.scrollIntoView(true);
-}
+
