7759ad0cefebe98f3222f80c61d9a433f3db5fa9
ThePywon <79721537+ThePywon@users.noreply.github.com>
2021-06-27 15:10:36 -0400

    Update index.js

--- main/index.js
+++ main/index.js
@@ -1,1 +1,40 @@
+var panelElements = document.getElementsByClassName("element");
+var current = 0;
 
+function Start()
+{
+  updatePanel();
+}
+
+function showLeft()
+{
+  current--;
+  if(current < 0)
+    current = panelElements.length-1;
+
+  updatePanel();
+}
+
+function showRight()
+{
+  current++;
+  if(current >= panelElements.length)
+    current = 0;
+
+  updatePanel();
+}
+
+function updatePanel()
+{
+  for(var i = 0; i < panelElements.length; i++)
+    if(i != current)
+      panelElements[i].style.visibility = "hidden";
+    else
+      panelElements[i].style.visibility = "visible";
+}
+
+var Update = setInterval(function(){
+
+},50);
+
+Start();
