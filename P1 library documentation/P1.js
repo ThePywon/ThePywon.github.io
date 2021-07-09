5c811f1b4961d2011237ded42c078c94152e3f6b
ThePywon <79721537+ThePywon@users.noreply.github.com>
2021-07-09 18:18:54 -0400

    Update P1.js

--- P1 library documentation/P1.js
+++ P1 library documentation/P1.js
@@ -1,1 +1,29 @@
+var slides = document.getElementsByClassName("slide");
+var boards = document.getElementsByClassName("board");
 
+for(var i = 0; i < slides.length; i++)
+  slides[i].style.display = "none";
+
+for(var i = 0; i < boards.length; i++)
+{
+  var size = 0;
+  var titles = boards[i].getElementsByClassName("titles")[0].getElementsByTagName("div");
+  for(var _i = 0; _i < titles.length; _i++)
+    size+=150;
+  boards[i].style.height = size + "px";
+}
+
+function show(index)
+{
+  for(var i = 0; i < slides.length; i++)
+  {
+    slides[i].style.display = "none";
+    if(index == i)
+    {
+      slides[i].style.display = "block";
+      slides[i].scrollIntoView();
+    }
+  }
+}
+
+show(8);
